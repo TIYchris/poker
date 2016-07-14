103012acc94230739e85617f76d06249f1589a9f
Chris Bergstrom <chris@snapcare.io>
2016-07-13 21:29:04 -0700

    Prepping for heroku

---
+++ src/app.js
@@ -0,0 +1,36 @@
+var $ = require('jquery');
+var WinLosses = require('./collections/collections_data');
+var WinLoss = require('./models/model_data');
+var CasinoTemplate = require('./templates/gambling.html');
+
+$(document).ready(function() {
+
+    var $form = $("#statementForm");
+
+    $form.on('submit', function(e) {
+        e.preventDefault();
+
+        var winloss = new WinLoss();
+
+        winloss.set({
+            'casino': $("#casino").val(),
+            'day': $("#day").val(),
+            'time': $("#time").val(),
+            'played': $("#played").val(),
+            'win': $("#win").val()
+        });
+
+        winloss.save();
+    });
+
+
+    WinLosses.fetch({
+    	success: function (collection) {
+    		var data = {casinos:collection.toJSON()}
+    		$("#app").html(CasinoTemplate(data));
+    	}
+    })
+
+
+
+});
